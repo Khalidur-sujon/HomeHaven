1d720a51f1ed3b5abf9588eb0b5b52a9b0e330fe
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-10 21:33:42 +0600

    create listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,1 +1,12 @@
-export const createListing = async (req, res, next) => {};
+import Listing from "../models/listing.model.js";
+
+//API
+//ROUTE:   /api/listing/create
+export const createListing = async (req, res, next) => {
+	try {
+		const listing = await Listing.create(req.body);
+		return res.status(201).json(listing);
+	} catch (error) {
+		next(error);
+	}
+};
