b28070ad540ff82500898614ba19ad65e104fb03
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-10 18:20:09 +0600

    added private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import Header from "./components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import { Home, About, Profile, SignIn, SignUp, CreateListing } from "./pages";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
 	return (
@@ -15,8 +16,13 @@ const App = () => {
 					<Route path="/about" element={<About />} />
 					<Route path="/sign-in" element={<SignIn />} />
 					<Route path="/sign-up" element={<SignUp />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/create-listing" element={<CreateListing />} />
+					<Route element={<PrivateRoute />}>
+						<Route path="/profile" element={<Profile />} />
+						<Route
+							path="/create-listing"
+							element={<CreateListing />}
+						/>
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		</>
