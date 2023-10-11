3f9aef2f0bf21a6d0c7ededce998963bde82dfdd
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-12 04:40:41 +0600

    configure listing route for delete item

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import customError from "../utils/customError.js";
 
 //API
 //ROUTE:   /api/listing/create
@@ -6,6 +7,27 @@ export const createListing = async (req, res, next) => {
 	try {
 		const listing = await Listing.create(req.body);
 		return res.status(201).json(listing);
+	} catch (error) {
+		next(error);
+	}
+};
+
+//API
+//ROUTE: /api/listing/deleteListing/:id
+export const deleteListingItem = async (req, res, next) => {
+	try {
+		//find the listing item
+		const listing = await Listing.findById(req.params.id);
+		if (!listing) {
+			return next(customError(404, "Listing not found"));
+		}
+		// if listing found, then verify the user
+		if (req.user.id !== listing.userRef) {
+			return next(customError(401, "You can delete only your listing"));
+		}
+		//if everything goes well then find the item and delete that
+		await Listing.findByIdAndDelete(req.params.id);
+		res.status(200).json("Delete Successfull");
 	} catch (error) {
 		next(error);
 	}
