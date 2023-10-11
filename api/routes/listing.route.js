84e189e84a38bedde4c35e3f5dade9ad3eb17ba7
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-12 04:39:59 +0600

    create delete listing item api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,16 @@
 //external imports
 import express from "express";
 //internal imports
-import { createListing } from "../controllers/listing.controller.js";
+import {
+	createListing,
+	deleteListingItem,
+} from "../controllers/listing.controller.js";
 import verifyUser from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
+router.delete("/deleteListing/:id", verifyUser, deleteListingItem);
 
 //export
 export default router;
