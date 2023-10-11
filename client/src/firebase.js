fe986d8d50313104109fe50f85b787cf20330aae
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-11 10:47:06 +0600

    initialize firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAZUbTtB_KJZD4Cc7fqluysp1P6m8f_ckw",
+	authDomain: "homeheaven-28c23.firebaseapp.com",
+	projectId: "homeheaven-28c23",
+	storageBucket: "homeheaven-28c23.appspot.com",
+	messagingSenderId: "749695873380",
+	appId: "1:749695873380:web:50d92befcedd1df869ca71",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
