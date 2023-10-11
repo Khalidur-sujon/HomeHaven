d3eccfd8272fa53e897a1b87d48319c1bf01d5d4
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-11 14:44:24 +0600

    complete listing image upload functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,4 +1,100 @@
+import { useState } from "react";
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
 const CreateListing = () => {
+	const [files, setfiles] = useState([]);
+	const [formData, setformData] = useState({
+		imageUrls: [],
+		name: "",
+		description: "",
+		address: "",
+		type: "rent",
+		bedrooms: 1,
+		bathrooms: 1,
+		regularPrice: 50,
+		discountPrice: 0,
+		offer: false,
+		parking: false,
+		furnished: false,
+	});
+	const [imageUploadError, setimageUploadError] = useState(false);
+	const [uploading, setuploading] = useState(false);
+	const [error, seterror] = useState(false);
+	const [loading, setloading] = useState(false);
+
+	const handleImageSubmit = (e) => {
+		//check if there is any image and length of image files must be less than 7
+		if (files.length > 0 && files.length < 7) {
+			setuploading(true);
+			setimageUploadError(false);
+			const promises = [];
+
+			for (let i = 0; i < files.length; i++) {
+				promises.push(storeImage(files[i]));
+			}
+
+			Promise.all(promises)
+				.then((urls) => {
+					setformData({
+						...formData,
+						imageUrls: formData.imageUrls.concat(urls),
+					});
+					setimageUploadError(false);
+					setloading(false);
+				})
+				.catch((err) => {
+					setimageUploadError(
+						"Image upload Failed (2 MB max per image)"
+					);
+					setuploading(false);
+				});
+		} else {
+			setimageUploadError("You can upload maximum 6 images per listing");
+			setuploading(false);
+		}
+	};
+
+	const storeImage = async (file) => {
+		return new Promise((resolve, reject) => {
+			const storage = getStorage(app);
+			const filename = new Date().getTime() + file.name;
+			const storageRef = ref(storage, filename);
+			const uploadTask = uploadBytesResumable(storageRef, file);
+
+			uploadTask.on(
+				"state_changed",
+				(snapshot) => {
+					const progress =
+						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log(`Upload is ${progress}% done`);
+				},
+				(error) => {
+					reject(error);
+				},
+				//get the download link
+				() => {
+					getDownloadURL(uploadTask.snapshot.ref).then(
+						(downloadUrl) => resolve(downloadUrl)
+					);
+				}
+			);
+		});
+	};
+
+	//remove listing images
+	const handleRemoveImage = (index) => {
+		setformData({
+			...formData,
+			imageUrls: formData.imageUrls.filter((url, i) => i !== index),
+		});
+	};
+
 	return (
 		<div className="max-w-4xl p-3 mx-auto">
 			<h1 className="font-bold text-3xl text-center my-7">
@@ -129,17 +225,47 @@ const CreateListing = () => {
 							accept="image/*"
 							multiple
 							className="cursor-pointer w-full border border-gray-300 p-3 rounded-lg"
+							onChange={(e) => setfiles(e.target.files)}
 						/>
 
 						<button
 							type="button"
+							disabled={uploading}
+							onClick={handleImageSubmit}
 							className="border rounded-lg border-green-700 text-green-700 font-normal p-3"
 						>
-							Upload
+							{uploading ? "Uploading ..." : "Upload"}
 						</button>
 					</div>
-					<button className="bg-slate-700 text-white p-3 uppercase rounded-lg">
-						Create Listing
+					{/* show error messge */}
+					<p className="text-red-700 text-sm">
+						{imageUploadError && imageUploadError}
+					</p>
+					{/* show uploaded image */}
+					{formData.imageUrls.length > 0 &&
+						formData.imageUrls.map((url, index) => (
+							<div
+								key={index}
+								className="border flex items-center justify-between p-3"
+							>
+								<img
+									src={url}
+									alt="lisintg_image"
+									className="w-20 h-20 object-contain rounded-lg"
+								/>
+								<button
+									className="p-2 rounded-lg bg-red-700 text-white hover:opacity-75"
+									onClick={() => handleRemoveImage(index)}
+								>
+									Delete
+								</button>
+							</div>
+						))}
+					<button
+						disabled={loading || uploading}
+						className="bg-slate-700 text-white p-3 uppercase rounded-lg"
+					>
+						{loading ? "Creating ... " : "Create Listing "}
 					</button>
 				</div>
 			</form>
