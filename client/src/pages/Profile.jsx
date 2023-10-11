75e8c2be42b1178a8597e2d5fcc8e5b3ede2a88a
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-12 00:39:57 +0600

    add show UI for show lisitings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,8 +13,11 @@ import {
 const Profile = () => {
 	const dispatch = useDispatch();
 	const [formData, setformData] = useState({});
+	const [showListingsError, setshowListingsError] = useState(false);
+	const [userListings, setuserListings] = useState([]);
 	const navigate = useNavigate();
 
+	console.log(userListings);
 	const { currentUser, loading, error } = useSelector((state) => state.user);
 
 	const handleOnChange = (e) => {
@@ -60,6 +63,26 @@ const Profile = () => {
 			navigate("/sign-in");
 		} catch (error) {
 			console.log(error);
+		}
+	};
+
+	//handle show listings
+	const handleShowListings = async () => {
+		try {
+			setshowListingsError(false);
+
+			const res = await fetch(`/api/user/getListings/${currentUser._id}`);
+
+			const data = await res.json();
+
+			if (data.success === false) {
+				setshowListingsError(false);
+				return;
+			}
+
+			setuserListings(data);
+		} catch (error) {
+			setshowListingsError(error);
 		}
 	};
 
@@ -114,6 +137,60 @@ const Profile = () => {
 					Sign out
 				</span>
 			</div>
+			<button
+				className="text-green-700 opacity-85 text-center font-medium text-xl hover:scale-105 hover:opacity-100 transition-all duration-300"
+				onClick={handleShowListings}
+			>
+				Show Listings
+			</button>
+			{/* show listing error message */}
+			<p className="text-sm text-red-700">
+				{showListingsError ? "Error showing listings" : " "}
+			</p>
+			{/* show all the listings */}
+			{userListings && userListings.length > 0 && (
+				<div className="flex flex-col gap-4">
+					<h2 className="font-bold text-2xl text-center mt-7">
+						Your Listings
+					</h2>
+
+					{userListings.map((listing) => (
+						<div
+							key={listing._id}
+							className="border rounded-lg p-3 flex justify-between items-center gap-4"
+						>
+							{/* image link */}
+							<Link to={`/listing/${listing._id}`}>
+								<img
+									src={listing.imageUrls[0]}
+									alt="listing cover"
+									className="h-16 w-16 object-contain aspect-square"
+								/>
+							</Link>
+							{/* listing title link */}
+							<Link
+								className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+								to={`/listing/${listing._id}`}
+							>
+								<p>{listing.name}</p>
+							</Link>
+
+							{/* listing edit/delete link */}
+
+							<div className="flex flex-col  item-center gap-2">
+								<button className="text-red-700 uppercase">
+									Delete
+								</button>
+								<Link to={`/update-listing/${listing._id}`}>
+									<button className="text-green-700 uppercase">
+										Edit
+									</button>
+								</Link>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
