1f0f9d5b46e8c00546a1fae3bcc3ab818c153e9b
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-14 13:11:02 +0600

    update header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,32 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+	const [searchTerm, setsearchTerm] = useState("");
+	const navigate = useNavigate();
+
+	//handle search
+	const handleSubmitHandler = (e) => {
+		e.preventDefault();
+
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("searchTerm", searchTerm);
+		const searchQuery = urlParams.toString();
+		//navigate to search page
+		navigate(`/search?${searchQuery}`);
+	};
+
+	// logic is , when the serchterm changes, we have to update it in the search box also
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search);
+		const searchTermFromUrl = urlParams.get("searchTerm");
+
+		if (searchTermFromUrl) {
+			setsearchTerm(searchTermFromUrl);
+		}
+	}, [location.search]);
+
 	return (
 		<header className="bg-slate-200  shadow-md">
 			<div className="max-w-6xl flex items-center justify-between  p-3 mx-auto">
@@ -14,15 +39,23 @@ const Header = () => {
 				</Link>
 
 				{/* search */}
-				<from className="bg-slate-100 p-3 rounded-lg flex items-center">
+				<form
+					className="bg-slate-100 p-3 rounded-lg flex items-center"
+					onSubmit={handleSubmitHandler}
+				>
 					<input
 						type="text"
 						id="search"
 						placeholder="Search here ... "
 						className="bg-transparent w-24 sm:w-64 focus:outline-none placeholder:italic"
+						value={searchTerm}
+						onChange={(e) => setsearchTerm(e.target.value)}
 					/>
-					<FaSearch className="text-slate-600 cursor-pointer ml-2" />
-				</from>
+
+					<button>
+						<FaSearch className="text-slate-600 cursor-pointer ml-2" />
+					</button>
+				</form>
 
 				{/* nav links */}
 				<ul className="flex gap-4 ">
