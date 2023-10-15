261512dd6bf8458f1cfa92b1eeb9a78ae93fdfa9
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-15 18:25:39 +0600

    fixing typing mistake

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/listing", listingRoutes);
 
-app.use(express.static(path, join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
