8beeb4c03f3037a074ed59ab3c5bb47a0e4860e2
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-22 07:48:13 +0600

    fixing layout

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const Footer = () => {
 	return (
-		<footer className="bg-slate-300 h-8 w-full text-center text-black py-2 flex items-center justify-center relative left-0 right-0 bottom-0">
+		<footer className="bg-slate-300 h-8 w-full text-center text-black py-2 flex items-center justify-center fixed bottom-0">
 			<p>
 				&copy;{" "}
 				<span className="italic font-medium">Khaldiur Rahman</span>
